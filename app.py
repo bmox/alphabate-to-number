e1c23ecc765407fd9f899c14c04bf1f3ab7eabdf
bmox <62886280+bmox@users.noreply.github.com>
2022-09-06 13:05:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,18 @@ for letter in string.ascii_uppercase:
     i+=1
     
 a = st.text_input('Enter The Word', 'ABCD')
-my_str=[]
-my_number=[]
-for i in str(a):
-    my_str.append(i.upper())
-    my_number.append(data[i.upper()])
-
-data = {'alphbate': my_str, 'number': my_number}
-df = pd.DataFrame.from_dict(data)
 res = any(chr.isdigit() for chr in str(a))
 if st.button('Display Number'):
     if res:
         st.write("Enter a string")
-    else:
+    else:         
+        my_str=[]
+        my_number=[]
+        for i in str(a):
+            my_str.append(i.upper())
+            my_number.append(data[i.upper()])
+
+        data = {'alphbate': my_str, 'number': my_number}
+        df = pd.DataFrame.from_dict(data)
+        res = any(chr.isdigit() for chr in str(a))
         st.table(df)
